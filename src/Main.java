05ecf1ad2432fbb571655671c73c6f96c8a43c78
Pavel <pavel.0gu@.com>
2023-12-02 14:59:07 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15_958;
+
+        int miles = price / 20;
+        System.out.println("Бонус: " + miles);
+    }
+}
